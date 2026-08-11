4c050c5065766dad032d629da9309c4f1ee7d0b1
hodienhung <lynguyen20013@gmail.com>
2026-08-11 17:10:09 +0700

    Create PageResponseDTO.java

---
+++ PageResponseDTO.java
@@ -0,0 +1,33 @@
+package com.example.demo.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageResponse<T> {
+    private List<T> items;
+    private int page;
+    private int size;
+    private long totalItems;
+    private int totalPages;
+    private boolean isLast;
+
+    public static <T> PageResponse<T> from(Page<T> page) {
+        return PageResponse.<T>builder()
+                .items(page.getContent())
+                .page(page.getNumber())
+                .size(page.getSize())
+                .totalItems(page.getTotalElements())
+                .totalPages(page.getTotalPages())
+                .isLast(page.isLast())
+                .build();
+    }
+}
